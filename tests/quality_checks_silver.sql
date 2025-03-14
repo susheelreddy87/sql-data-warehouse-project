5507cc6adbe382b85c3028537ab14795d1f8fb6d
Susheel Anugu <aenugusairam@gmail.com>
2025-03-14 15:47:03 -0400

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,27 +1,17 @@
 /*
-===============================================================================
-Quality Checks
-===============================================================================
-Script Purpose:
-    This script performs various quality checks for data consistency, accuracy, 
-    and standardization across the 'silver' layer. It includes checks for:
+Script Quality Checking Purpose:
+    To Check for 
     - Null or duplicate primary keys.
     - Unwanted spaces in string fields.
     - Data standardization and consistency.
     - Invalid date ranges and orders.
     - Data consistency between related fields.
-
-Usage Notes:
-    - Run these checks after data loading Silver Layer.
-    - Investigate and resolve any discrepancies found during the checks.
-===============================================================================
 */
 
 -- ====================================================================
 -- Checking 'silver.crm_cust_info'
 -- ====================================================================
--- Check for NULLs or Duplicates in Primary Key
--- Expectation: No Results
+-- Checking for NULLs or Duplicates in Primary Key
 SELECT 
     cst_id,
     COUNT(*) 
@@ -44,8 +34,8 @@ FROM silver.crm_cust_info;
 -- ====================================================================
 -- Checking 'silver.crm_prd_info'
 -- ====================================================================
--- Check for NULLs or Duplicates in Primary Key
--- Expectation: No Results
+-- Checking for NULLs or Duplicates in Primary Key
+
 SELECT 
     prd_id,
     COUNT(*) 
@@ -53,7 +43,7 @@ FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL;
 
--- Check for Unwanted Spaces
+-- Checking for Unwanted Spaces
 -- Expectation: No Results
 SELECT 
     prd_nm 
@@ -82,8 +72,7 @@ WHERE prd_end_dt < prd_start_dt;
 -- ====================================================================
 -- Checking 'silver.crm_sales_details'
 -- ====================================================================
--- Check for Invalid Dates
--- Expectation: No Invalid Dates
+-- Checking for Invalid Dates
 SELECT 
     NULLIF(sls_due_dt, 0) AS sls_due_dt 
 FROM bronze.crm_sales_details
@@ -92,16 +81,16 @@ WHERE sls_due_dt <= 0
     OR sls_due_dt > 20500101 
     OR sls_due_dt < 19000101;
 
--- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
--- Expectation: No Results
+-- Checking for Invalid Date Orders (Order Date > Shipping/Due Dates)
+
 SELECT 
     * 
 FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt 
    OR sls_order_dt > sls_due_dt;
 
--- Check Data Consistency: Sales = Quantity * Price
--- Expectation: No Results
+-- Checking Data Consistency: Sales = Quantity * Price
+
 SELECT DISTINCT 
     sls_sales,
     sls_quantity,
@@ -119,8 +108,7 @@ ORDER BY sls_sales, sls_quantity, sls_price;
 -- ====================================================================
 -- Checking 'silver.erp_cust_az12'
 -- ====================================================================
--- Identify Out-of-Range Dates
--- Expectation: Birthdates between 1924-01-01 and Today
+-- Identifying Out-of-Range Dates
 SELECT DISTINCT 
     bdate 
 FROM silver.erp_cust_az12
@@ -144,8 +132,7 @@ ORDER BY cntry;
 -- ====================================================================
 -- Checking 'silver.erp_px_cat_g1v2'
 -- ====================================================================
--- Check for Unwanted Spaces
--- Expectation: No Results
+-- Checking for Unwanted Spaces
 SELECT 
     * 
 FROM silver.erp_px_cat_g1v2
